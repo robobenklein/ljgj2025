4994d0d5760b6bcbaacfc446bea0041d96eb7000
Ben Klein <robobenklein@gmail.com>
2025-12-17 02:45:03 -0500

    fixin grammar, now have conditional control flow

--- paperwork_lang/parser.py
+++ paperwork_lang/parser.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from dataclasses import dataclass
 from typing import List, Optional
 
+import bpython
 import lark
 from lark import Lark, ast_utils, Transformer, v_args
 from .assets import module_dir
@@ -13,12 +14,19 @@ ebnf_file = module_dir / 'paperwork.lark'
 with ebnf_file.open('rt') as f:
     ebnf_definition = f.read()
 
-llang = Lark(ebnf_definition)
+llang = Lark(ebnf_definition, parser='lalr')
 
 this_module = sys.modules[__name__]
 
+def on_parse_error(e):
+    # bpython.embed(locals(), banner="Post-Mortem Debugging:")
+    pass
+
 def parse(text):
-    return llang.parse(text)
+    return llang.parse(
+        text,
+        on_error=on_parse_error,
+    )
 
 class _Ast(ast_utils.Ast):
     pass
@@ -28,10 +36,6 @@ class _Line(_Ast):
 
 class _Instruction(_Line):
     pass
-
-@dataclass
-class Comment(_Line):
-    comment_text: str
 
 @dataclass
 class Label(_Line):
@@ -64,30 +68,56 @@ class InsDrop(_Instruction):
     parcel_type: str
     parcel_identifier: Optional[int] = None
 
+@dataclass
+class TestSubject(_Ast):
+    subject_type: str
+    subject_property: str
+
+@dataclass
+class TestCondition(_Ast):
+    test_subject: TestSubject
+    test_value: Optional[int | str] = None
+
+@dataclass
+class InsWhen(_Instruction):
+    test_condition: TestCondition
+    instruction: _Instruction
+
+    def __str__(self):
+        return f"InsWhen(\ncondition={self.test_condition},\ninstruction={self.instruction})".replace('\n', '\n\t')
+
+@v_args(meta=True)
 class ToAst(Transformer):
     # Define extra transformation functions, for rules that don't correspond to an AST class.
-    def _convert_id_lower(self, t):
+    def _convert_id_lower(self, m, t):
         return t[0].value.lower()
-    
-    def _convert_int(self, t):
+
+    def _convert_int(self, m, t):
         return int(t[0].value)
+
+    def _convert_string(self, m, t):
+        return t[0].value
 
     location_identifier = _convert_id_lower
     location_type = _convert_id_lower
     parcel_type = _convert_id_lower
     parcel_identifier = _convert_int
+    # INT = _convert_int
 
-    def label_name(self, t):
-        return t[0].value
-
-    def comment_text(self, s):
-        t[0].value
+    label_name = _convert_string
+    property_name = _convert_string
+    # comment = _convert_string
 
     @v_args(inline=True)
     def start(self, x):
         # print(f"ast start: {x}, {a}, {kw}")
         return x
 
+    def test_value(self, m, x):
+        try:
+            return int(x[0].value)
+        except ValueError:
+            return x[0].value
 
 transformer = ast_utils.create_transformer(this_module, ToAst())
 
@@ -100,9 +130,9 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     with args.program.open('rt') as f:
-        ptree = llang.parse(f.read())
+        ptree = parse(f.read())
         print(ptree.pretty())
-    
+
     ast = tree_to_ast(ptree)
     for stmt in ast.lines:
         print(stmt)
